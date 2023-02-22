0a875e4ec90b956c174eb7e587908e847211157b
lihaoyu <lhy0825@outlook.com>
2023-02-23 01:39:59 +0800

    feat: 完成手写promise实现

--- diyPromsie.js
+++ diyPromsie.js
@@ -1,12 +1,22 @@
 const MyPromise = require('./myPromise')
-const p = new MyPromise((resolve, reject) => {
-  // setTimeout(() => {
-    resolve(123)
-  // }, 2000)
-}).then(value => {
-  console.log(123)
-  return p
-})
+// const p = new MyPromise((resolve, reject) => {
+//   // throw new Error('执行器错误')
+//   // resolve(123)
+//   // reject(123)
+//   setTimeout(() => {
+//     resolve('成功')
+//   }, 2000)
+// }).then(value => {
+//   console.log(123, 'aaa')
+//   throw new Error('successCallback Error')
+// }, error => {
+//   console.log(error)
+//   return 10000
+// }).then(value => {
+//   console.log(value, 456)  
+// }, error => {
+//   console.log(error.message)
+// })
 // p.then((res) => {
 //   console.log(res)
 //   return 456
@@ -21,3 +31,49 @@ const p = new MyPromise((resolve, reject) => {
 //   console.log(123)
 //   return p1
 // })
+
+// const p1 = new MyPromise((resolve, reject) => {
+//   reject(2000)
+// })
+// .then()
+// .then()
+// .then(
+//   value => console.log(value),
+//   error => console.log(error)
+// )
+
+// MyPromise.all([1, 
+//   new MyPromise((resolve) => {
+//     setTimeout(res => {
+//       resolve(33333)
+//     }, 2000)
+//   }), 
+// 3]).then(console.log)
+// MyPromise.resolve(1).then(console.log)
+// MyPromise.resolve(new MyPromise(resolve => {
+//   setTimeout(() => resolve('async'), 3000)
+// })).then(console.log)
+
+const p1 = new MyPromise((resolve, reject) => {
+  // resolve('成功')
+  reject('失败')
+  // setTimeout(() => resolve('timeout'), 2000)
+})
+const p2 = new MyPromise((resolve, reject) => {
+  // resolve('成功')
+  // reject('失败')
+  setTimeout(() => {
+    resolve('timeout2')
+    console.log('p2')
+  }, 2000)
+})
+
+// p1.finally(() => {
+//   console.log('finally')
+//   return p2
+// }).then(value => {
+//   console.log(value)
+// }, error => {
+//   console.log(error)
+// })
+p1.then().then().catch(console.log)
--- myPromise.js
+++ myPromise.js
@@ -22,7 +22,7 @@ class MyPromise {
       this.status = FULFILLED
       this.value = value
       while(this.successCallbacks.length) {
-        this.successCallbacks.shift()(value)
+        this.successCallbacks.shift()()
       }
     }
   }
@@ -31,44 +31,100 @@ class MyPromise {
       this.status = REJECTED
       this.error = error
       while(this.failCallbacks.length) {
-        this.failCallbacks.shift()(error)
+        this.failCallbacks.shift()()
       }
     }
   }
   constructor(executor) {
     this.status = PENDING
-    executor(this.resolve, this.reject)
+    try {
+      executor(this.resolve, this.reject)
+    } catch (error) {
+      this.reject(error)
+    }
   }
   then(successCallback, failCallback) {
+    successCallback = successCallback ? successCallback : value => value
+    failCallback = failCallback ? failCallback : error => { throw error }
     let returnPromise = new Promise((resolve, reject) => {
       if(this.status === FULFILLED) {
         setTimeout(() => {
-          let x = successCallback(this.value)
-          resolvePromise(returnPromise, x, resolve, reject)
+          try {
+            let x = successCallback(this.value)
+            resolvePromise(returnPromise, x, resolve, reject)
+          } catch(error) {
+            reject(error)
+          }
         }, 0)
       } else if(this.status === REJECTED) {
-        failCallback && failCallback(this.error)
+        setTimeout(() => {
+          try {
+            let x = failCallback(this.error)
+            resolvePromise(returnPromise, x, resolve, reject)
+          } catch(error) {
+            reject(error)
+          }
+        }, 0)
       } else {
-        successCallback && this.successCallbacks.push(successCallback)
-        failCallback && this.failCallbacks.push(failCallback)
+        this.successCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              let x = successCallback(this.value)
+              resolvePromise(returnPromise, x, resolve, reject)
+            } catch(error) {
+              reject(error)
+            }
+          }, 0)
+        })
+        this.failCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              let x = failCallback(this.error)
+              resolvePromise(returnPromise, x, resolve, reject)
+            } catch(error) {
+              reject(error)
+            }
+          }, 0)
+        })
       }
     })
     return returnPromise
   }
-  catch (errorFn) {
-    this.catchCallback = errorFn
+  catch (callback) {
+    return this.then(undefined, callback)
   }
-  // static resolve (value) {
-  //   return new DiyPromise((resolve, reject) => resolve(value))
-  // }
-  // static reject(error) {
-  //   return new DiyPromise((resolve, reject) => reject(error))
-  // }
-  // static all (instances) {
-
-  // }
-  // static race (instances) {
-
-  // }
+  finally (callback) {
+    return this.then((value) => {
+      return MyPromise.resolve(callback()).then(() => value)
+    }, (reason) => {
+      console.log('finally then fail', reason)
+      return MyPromise.resolve(callback()).then(() => { throw reason })
+    })
+  }
+  static resolve (value) {
+    if(value instanceof MyPromise) return value
+    return new MyPromise(resolve => resolve(value))
+  }
+  static all (pArray) {
+    return new MyPromise((resolve, reject) => {
+      const result = new Array(pArray.length).fill(undefined)
+      let count = 0
+      for(let i = 0; i < pArray.length; i ++) {
+        const current = pArray[i]
+        if(current instanceof MyPromise) {
+          current.then((value) => {
+            result[i] = value
+            count++
+            if(count === pArray.length) resolve(result)
+          }).catch(error => {
+            reject(error)
+          })
+        } else {
+          result[i] = current
+          count++
+        }
+      }
+    })
+  }
 }
 module.exports = MyPromise
